1d9af188e513956b32d0bfe1a4ba0435efd2e698
kohp27 <ohashi@sonicgarden.jp>
2026-08-28 15:41:26 +0900

    iOSにCODE_SIGNING_ALLOWED=NOを設定

--- shared/upload_ios.sh
+++ shared/upload_ios.sh
@@ -36,6 +36,7 @@ xcodebuild archive \
   -authenticationKeyPath "$AUTH_KEY_PATH" \
   -authenticationKeyID "$APP_STORE_CONNECT_KEY_ID" \
   -authenticationKeyIssuerID "$APP_STORE_CONNECT_ISSUER_ID" \
+  CODE_SIGNING_ALLOWED=NO \
   ${EXTRA_ARGS:-}
 
 xcodebuild -exportArchive \
